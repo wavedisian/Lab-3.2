e1713cbd5c761644a7e87964835dcc28baa30eb2
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-02-06 08:24:20 -0500

    merge done

--- Lab 3.2/src/ControlStructures/MergePartition.java
+++ Lab 3.2/src/ControlStructures/MergePartition.java
@@ -2,13 +2,57 @@ package ControlStructures;
 
 public class MergePartition {
 
-	public static int[] merge(int[] x, int[] y)
+	public static String[] merge(String[] x, String[] y)
 	{
-		
+		String[] combo = new String[x.length + y.length];
+		int xc = 0;
+		int yc = 0;
+		while(xc + yc < (x.length + y.length) - 2)
+		{
+			if(x[xc].compareTo(y[yc])>=0)
+			{
+				combo[xc + yc] = y[yc];
+				yc++;
+			}
+			if(x[xc].compareTo(y[yc])<0)
+			{
+				combo[xc + yc] = x[xc];
+				xc++;
+			}
+		}
+		return combo;
 	}
 	
-	public static int partition(int[] x)
+	public static int partition(String[] x)
 	{
-		
+		int P = 0;
+		int L = 1;
+		int R = x.length;
+		while(R > L)
+		{
+			if(x[R].compareTo(x[P])<0)
+			{
+				swap(x, P, R);
+			}
+		}
+	}
+	
+	public static void swap(int[] a, int x, int y)
+	{
+		int s = a[x];
+		a[x] = a[y];
+		a[y] = s;
+	}
+	public static void swap(double[] a, int x, int y)
+	{
+		double s = a[x];
+		a[x] = a[y];
+		a[y] = s;
+	}
+	public static void swap(String[] a, int x, int y)
+	{
+		String s = a[x];
+		a[x] = a[y];
+		a[y] = s;
 	}
 }
